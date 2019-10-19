d2c428a612fcfbe93b3536ee4133fc53b35eb2d1
AngryFennec <angryfennecus@gmail.com>
2019-10-19 17:39:18 +0300

    Добавляет пропсы

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import PropTypes from 'prop-types';
 
 const App = (props) => {
   const {gameTime, errorCount} = props;
@@ -7,6 +8,11 @@ const App = (props) => {
     gameTime = {gameTime}
     errorCount = {errorCount}
   />;
+};
+
+App.propTypes = {
+  gameTime: PropTypes.number,
+  errorCount: PropTypes.number,
 };
 
 export default App;
