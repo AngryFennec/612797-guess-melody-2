a0827edc013b83758a565c65ab25035d447b3314
AngryFennec <shellfix@gmail.com>
2019-10-22 17:12:22 +0300

    Снэпшот-тесты

---
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WelcomeScreen from './welcome-screen.jsx';
+import renderer from 'react-test-renderer';
+
+it(`WelcomeScreen component renders correctly`, () => {
+  const tree = renderer
+    .create(<WelcomeScreen
+      gameTime = {0}
+      errorCount = {0}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
